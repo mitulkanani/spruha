cf28ca4249411dd7f78e2701d960e631184ad082
smitchovatiya0701 <smitchovatiya96@gmail.com>
2024-03-28 18:29:42 +0530

    solve heading problem

--- shared/data/table/data-tables.tsx
+++ shared/data/table/data-tables.tsx
@@ -384,12 +384,12 @@ export const DataTables = () => {
     data,
   };
   useEffect(() => {
-    const element = document.querySelector(".sc-fHejqy.evaFvq.rdt_TableHeader");
+    const element = document.querySelector('[role="heading"]');
 
     // Check if the element exists
     if (element) {
       // Add CSS to hide the element
-      (element as HTMLElement).style.display = "none";
+      (element as HTMLElement).style.display = "none !important";
     } else {
       console.error("Element not found.");
     }
